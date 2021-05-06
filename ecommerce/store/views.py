0f3078d299b549bee5f439ca6369bf2b078ab134
aaqibb13 <aaqibb13@gmail.com>
2021-05-06 15:38:49 +0530

    Updated views.py

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
+
 # Create your views here.
 
 def store(request):
@@ -29,3 +31,6 @@ def checkout(request):
         order = {'get_cart_total':0, 'get_cart_items':0}
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+def UpdateItem(request):
+    return JsonResponse('Item was added', safe=False)
