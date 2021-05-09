2f0ef9f6a0f3184bb128f0ac6ed7a05d5d637e2f
aaqibb13 <aaqibb13@gmail.com>
2021-05-09 15:49:59 +0530

    Update views.py

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -39,7 +39,23 @@ def UpdateItem(request):
     productId = data['productId']
     action = data['action']
 
-    print('Action', action)
-    print('productId', productId)
+    print('Action:', action)
+    print('productId:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+    
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
 
     return JsonResponse('Item was added', safe=False)
