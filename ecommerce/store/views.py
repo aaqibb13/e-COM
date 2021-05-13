82015901c3a5b63e8b9d4d745bf6fe55e0e191d6
aaqibb13 <aaqibb13@gmail.com>
2021-05-14 00:22:19 +0530

    Updated views.py

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -7,8 +7,17 @@ from .models import *
 # Create your views here.
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        cartitems = order.get_cart_items()
+    
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)
 
 def cart(request):
