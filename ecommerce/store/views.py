d44e33cc3f4381f7e8aa4d123cad123074ea3c0c
aaqibb13 <aaqibb13@gmail.com>
2021-05-25 19:58:16 +0530

    Updated `views.py`

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -73,4 +73,5 @@ def UpdateItem(request):
     return JsonResponse('Item was added', safe=False)
 
 def processOrder(request):
+    print('Data:', request.body)
     return JsonResponse('Payment complete!', safe=False)
