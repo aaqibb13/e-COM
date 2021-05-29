b67d5f528366e09a5281ca7b3d7874b7b6b2a6e7
aaqibb13 <aaqibb13@gmail.com>
2021-05-29 22:17:24 +0530

    Remove `@csrf_exempt`

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -72,9 +72,7 @@ def UpdateItem(request):
         orderItem.delete()
 
     return JsonResponse('Item was added', safe=False)
-# from django.views.decorators.csrf import csrf_exempt
 
-# @csrf_exempt
 def processOrder(request):
     transaction_id = datetime.datetime.now().timestamp()
     data = json.loads(request.body)
