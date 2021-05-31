b9e014643e1c8986ec1e3448230f36805cb9615c
aaqibb13 <aaqibb13@gmail.com>
2021-05-31 15:12:04 +0530

    Add `addCookieItem` func for unauthenticated user

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -7,12 +7,34 @@ for(var i = 0; i < updateBtns.length; i++){
         console.log('productId:', productId, 'action:', action)
 
         console.log('USER:', user)
-        if(user === 'AnonymousUser'){
-            console.log('User not logged in')
+        if(user == 'AnonymousUser'){
+            addCookieItem(productId, action)
         }else{
             updateUserOrder(productId, action)
         }
     })
+}
+function addCookieItem(productId, action){
+    console.log('User not logged in...')
+    if(action == 'add'){
+        if(cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+        }else{
+            cart[productId]['quantity'] += 1
+        }
+    }
+    if(action == 'remove'){
+        cart[productId]['quantity'] -= 1
+
+        if(cart[productId]['quantity'] <= 0){
+            console.log('Remove Item')
+            delete cart[productId]
+        }
+    }
+    console.log('Cart:', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain;path=/"
+    location.reload()
+
 }
 
 function updateUserOrder(productId, action){
