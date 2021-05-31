8340ffb2a0f756e04f0d56a4031d69068877ac67
aaqibb13 <aaqibb13@gmail.com>
2021-05-31 15:28:05 +0530

    Add cart-total in `cart` view

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -13,9 +13,17 @@ def store(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+            print('Cart:', cart)
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
         cartItems = order['get_cart_items']
+
+        for i in cart:
+            cartItems += cart[i]["quantity"]
     
     products = Product.objects.all()
     context = {'products': products, 'cartItems': cartItems}
@@ -29,9 +37,17 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+            print('Cart:', cart)
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
         cartItems = order['get_cart_items']
+
+        for i in cart:
+            cartItems += cart[i]["quantity"]
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
